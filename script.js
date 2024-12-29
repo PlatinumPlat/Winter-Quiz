2e915d452891a301f7c54ca4599052355243fb95
PlatinumPlat <twinsophiapu@gmail.com>
2024-12-29 12:32:32 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,64 +1,16 @@
 const questions = [
-    {
-        question: "Mistletoe berries are what colour?",
-        options: {
-            A: "Red",
-            B: "Green",
-            C: "White",
-            D: "Black"
-        },
-        correctAnswer: "C"
-    },
-    {
-        question: "The city of Yakutsk receives the most snow in the world. Where is it?",
-        options: {
-            A: "Russia",
-            B: "Japan",
-            C: "Canada",
-            D: "Iceland"
-        },
-        correctAnswer: "A"
-    },
-    {
-        question: "Chionophobia means the fear of...",
-        options: {
-            A: "Icicles",
-            B: "Snow",
-            C: "Christmas",
-            D: "Santa"
-        },
-        correctAnswer: "B"
-    },
-    {
-        question: "Which city hosts the largest New Year's Eve party?",
-        options: {
-            A: "Rio de Janeiro",
-            B: "New York",
-            C: "Toronto",
-            D: "Moscow"
-        },
-        correctAnswer: "A"
-    },
-    {
-        question: "What was the highest-grossing Christmas movie of all time?",
-        options: {
-            A: "Elf",
-            B: "The Polar Express",
-            C: "Home Alone",
-            D: "How the Grinch Stole Christmas"
-        },
-        correctAnswer: "C"
-    }
+    {question: "Mistletoe berries are what colour?",options: {A: "Red",B: "Green",C: "White",D: "Black"},correctAnswer: "C"},
+    {question: "The city of Yakutsk receives the most snow in the world. Where is it?",options: {A: "Russia",B: "Japan",C: "Canada",D: "Iceland"},correctAnswer: "A"},
+    {question: "Chionophobia means the fear of...",options: {A: "Icicles",B: "Snow",C: "Christmas",D: "Santa"},correctAnswer: "B"},
+    {question: "Which city hosts the largest New Year's Eve party?",options: {A: "Rio de Janeiro",B: "New York",C: "Toronto",D: "Moscow"},correctAnswer: "A"},
+    {question: "What was the highest-grossing Christmas movie of all time?",options: {A: "Elf",B: "The Polar Express",C: "Home Alone",D: "How the Grinch Stole Christmas"},correctAnswer: "C"}
 ];
 
 let currentQuestion = 0;
 let score = 0;
 
 function loadQuestion() {
-    document.getElementById("answer A").disabled = false;
-    document.getElementById("answer B").disabled = false;
-    document.getElementById("answer C").disabled = false;
-    document.getElementById("answer D").disabled = false;
+    document.getElementById("answer A").disabled = document.getElementById("answer B").disabled = document.getElementById("answer C").disabled = document.getElementById("answer D").disabled = false;
     document.getElementById("next").disabled = true;
     const questionObj = questions[currentQuestion];
     document.getElementById("question").textContent = questionObj.question;
@@ -83,16 +35,10 @@ function checkAnswer(answer) {
     
     currentQuestion++;
     if (currentQuestion < 5) {
-        document.getElementById("answer A").disabled = true;
-        document.getElementById("answer B").disabled = true;
-        document.getElementById("answer C").disabled = true;
-        document.getElementById("answer D").disabled = true;
+        document.getElementById("answer A").disabled = document.getElementById("answer B").disabled = document.getElementById("answer C").disabled = document.getElementById("answer D").disabled = true;
         nextButton();
     } else {
-        document.getElementById("answer A").disabled = true;
-        document.getElementById("answer B").disabled = true;
-        document.getElementById("answer C").disabled = true;
-        document.getElementById("answer D").disabled = true;
+        document.getElementById("answer A").disabled = document.getElementById("answer B").disabled = document.getElementById("answer C").disabled = document.getElementById("answer D").disabled = true;
         document.getElementById('next').style.display="none";
         setTimeout(showFinalScore, 1500);
     }
@@ -101,11 +47,7 @@ function checkAnswer(answer) {
 
 
 function showFinalScore() {
-    document.getElementById("answer A").style.display="none";
-    document.getElementById("answer B").style.display="none";
-    document.getElementById("answer C").style.display="none";
-    document.getElementById("answer D").style.display="none";
-    document.getElementById("question").style.display="none";
+    document.getElementById("answer A").style.display=document.getElementById("answer B").style.display=document.getElementById("answer C").style.display=document.getElementById("answer D").style.display=document.getElementById("question").style.display="none";
     document.getElementById("result").textContent = `The Quiz is over! Your final score is: ${score} out of ${questions.length}.`;
     document.getElementById('img').classList.toggle('show');
     console.log("HI");
